3110126bfa0e73c68628d874fb8044c661e60486
datapip <p.a.jaeckle@gmail.com>
2026-07-13 18:05:29 +0200

    updated gtm snippet

--- shopify-setup/shopify-custom-pixel.js
+++ shopify-setup/shopify-custom-pixel.js
@@ -27,7 +27,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   gtmSnippetDev: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=-ohLEOY9C60-YHK7E_4OWA&gtm_preview=env-1&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=AIx1Q7TX0FU-y5mdVjE_Dg&gtm_preview=env-66&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-K84QTPLJ');`,
 
   // select consent(s) to load GTM,
